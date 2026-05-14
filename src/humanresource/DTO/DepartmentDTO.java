b4af811bee7496ef3586bfa359cc38a662c6483a
HongJunPyo0222 <widejunpyo@gmail.com>
2026-05-14 10:11:49 +0900

    feat: DepartmentDTO 개발

---
+++ src/humanresource/DTO/DepartmentDTO.java
@@ -0,0 +1,19 @@
+package humanresource.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DepartmentDTO {
+    private Long deptId;
+    private String deptName;
+    private String deptDesc;
+    private Long managerId;
+    private Long ParentDeptId;
+
+}
