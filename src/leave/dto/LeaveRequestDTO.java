d8d91ffe062664c557fb1932deeb83da75a6d170
woodgeon <ad4086@naver.com>
2026-05-15 09:25:48 +0900

    feat: 휴가 신청 DTO 추가

---
+++ src/leave/dto/LeaveRequestDTO.java
@@ -0,0 +1,24 @@
+package leave.dto;
+
+import global.types.CommonStatus;
+import global.types.LeaveType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LeaveRequestDTO {
+    private Long leaveRequestId;
+    private Long employeeId;
+    private String reason;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LeaveType leaveType;
+    private CommonStatus status;
+
+}
