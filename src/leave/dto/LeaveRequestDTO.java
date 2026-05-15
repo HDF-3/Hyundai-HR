037e3450ad3e278079c71e6dba512bf5dcc6c865
woodgeon <ad4086@naver.com>
2026-05-15 09:27:03 +0900

    chore: 사용하지 않는 라이브러리 제거

--- src/leave/dto/LeaveRequestDTO.java
+++ src/leave/dto/LeaveRequestDTO.java
@@ -4,7 +4,6 @@ import global.types.CommonStatus;
 import global.types.LeaveType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
