2a59e287be5bfbd90dba11c17d3ac2d92cb8022c
woodgeon <ad4086@naver.com>
2026-05-15 09:37:10 +0900

    feat: 현재년도의 연차만 조회하는 로직 추가

--- src/leave/dao/LeaveDAO.java
+++ src/leave/dao/LeaveDAO.java
@@ -10,49 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-/*
-public List<Emp> getEmpList() {
-        List<Emp> empList = new ArrayList<Emp>();
-        try (
-                Connection conn = ConnectionHelper.getConnection(DBType.ORACLE);
-                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM emp");
-                ResultSet rs = pstmt.executeQuery();
-                ){
-            while (rs.next()) {
-                Emp emp = new Emp();
-                emp.setEmpno(rs.getInt("empno"));
-                emp.setEname(rs.getString("ename"));
-                emp.setJob(rs.getString("job"));
-                emp.setDeptno(rs.getInt("deptno"));
-                empList.add(emp);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return empList;
-    }
-    public Emp getEmpById(int empno) {
-        Emp emp = new Emp();
-        try (
-                Connection conn = ConnectionHelper.getConnection(DBType.ORACLE);
-                PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM emp where empno=?");
-                ){
-            pstmt.setInt(1, empno);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                emp.setEmpno(rs.getInt("empno"));
-                emp.setEname(rs.getString("ename"));
-                emp.setJob(rs.getString("job"));
-                emp.setDeptno(rs.getInt("deptno"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return emp;
-    }
- */
 public class LeaveDAO {
     public List<AnnualLeaveDTO> findAnnualLeaveListByEmployeeId(Long employeeId) {
         List<AnnualLeaveDTO> list = new ArrayList<>();
@@ -83,5 +40,32 @@ public class LeaveDAO {
             e.printStackTrace();
         }
         return list;
+    }
+    public AnnualLeaveDTO findAnnualLeaveById(Long employeeId) {
+        AnnualLeaveDTO dto = new AnnualLeaveDTO();
+        String sql = "select EMP_ID, GRANTED_ANNUAL_LEAVE, USED_ANNUAL_LEAVE, REMAINING_ANNUAL_LEAVE, IS_ACTIVE, GRANTED_AT, EXPIRED_AT " +
+                "from ANNUAL_LEAVE " +
+                "where EMP_ID = ? and IS_ACTIVE = 'Y'";
+        try (
+                Connection conn = ConnectionHelper.getConnection(DBType.ORACLE);
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ){
+            pstmt.setLong(1, employeeId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    dto.setEmployeeId(rs.getLong("EMP_ID"));
+                    dto.setGrantedAnnualLeave(rs.getDouble("GRANTED_ANNUAL_LEAVE"));
+                    dto.setUsedAnnualLeave(rs.getDouble("USED_ANNUAL_LEAVE"));
+                    dto.setRemainingAnnualLeave(rs.getDouble("REMAINING_ANNUAL_LEAVE"));
+                    dto.setIsActive(rs.getString("IS_ACTIVE").charAt(0));
+                    dto.setGrantedAt(rs.getDate("GRANTED_AT").toLocalDate());
+                    dto.setExpiredAt(rs.getDate("EXPIRED_AT").toLocalDate());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dto;
     }
 }
